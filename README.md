d94b1c6abf398c66dc286c1e169c1cf2830e402b
Pander <pander@users.sourceforge.net>
2016-03-24 17:33:24 +0100

    First retrieval

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # afvalophaaldata
 Kalender met afvalophaaldata in Nederland voor postcode en huisnummer
+
+Maintenance
+-----------
+
+Calendars that have been generated can be validated by:
+* http://severinghaus.org/projects/icv/?url=https%3A%2F%2Fraw.githubusercontent.com%2FPanderMusubi%2Fafvalophaaldata%2Fmaster%2F3521VX-90-BS.ics
+* http://severinghaus.org/projects/icv/?url=https%3A%2F%2Fraw.githubusercontent.com%2FPanderMusubi%2Fafvalophaaldata%2Fmaster%2F3524DZ-172.ics
--- retrieve.py
+++ retrieve.py
@@ -5,13 +5,13 @@
 # license: MIT
 # date: 2016-03-24
 
-from datetime import datetime
-from sys import argv
+from datetime import datetime, timedelta
+from random import uniform
 from time import sleep
 from urllib import request
 
 
-def monthToNumber(month):
+def month_to_number(month):
     if month == 'januari':
         return '01'
     elif month == 'februari':
@@ -39,14 +39,24 @@ def monthToNumber(month):
     else:
         return ''
 
-now = datetime.now()
-stamp = now.strftime('%Y%m%d%H%M')
-year_number = now.strftime('%Y')
-print('{}'.format(stamp))
+utcnow = datetime.utcnow()
+year = utcnow.strftime('%Y')
+dtstamp = utcnow.strftime('%Y%m%dT%H%M%SZ')
+
+collection_header = ''
+event_header = open('templates/event-header.txt', 'r')
+for line in event_header.readlines():
+    collection_header += line.replace('DTSTAMP:', 'DTSTAMP:{}'.format(dtstamp))
+
+collection_footer = ''
+event_footer = open('templates/event-footer.txt', 'r')
+for line in event_footer.readlines():
+    collection_footer += line
+
 for address in open('addresses.tsv', 'r'):
     address = address[:-1].replace('\t', '/')
     if address != '':
-        sleep(1)
+        sleep(uniform(1, 3))
         print(address)
         url = 'http://www.mijnafvalwijzer.nl/nl/{}/'.format(address)
 
@@ -56,20 +66,31 @@ for address in open('addresses.tsv', 'r'):
             print('ERROR')
 
         data = data.split('\n')
-        calendar = open('{}.ics'.format(address.replace('/', '-')), 'w')
-        calendar.write('{}\n'.format(stamp))
-		
+        calendar = open('calendars/{}.ics'.format(address.replace('/', '-')), 'w')
+
+        calendar_header = open('templates/calendar-header.txt', 'r')
+        for line in calendar_header.readlines():
+            calendar.write(line)
+
         for line in data:
             if '<a href="#waste-' in line:
-                collection = line.split('title="')[1]
-                second = collection.split('"><p class="')
-                collection = second[0]
+                name = line.split('title="')[1]
+                second = name.split('"><p class="')
+                name = second[0].replace(',', '\,')
                 second = second[1].replace('<br />', '')
-                second = second.replace('gft">', '')
-                second = second.replace('kerstbomen">', '')
-                second = second.replace('papier">', '')
-                second = second.replace('pmd">', '')
-                (day, day_number, month) = second.split(' ')
-                month_number = monthToNumber(month)
-                calendar.write('{}\n'.format(collection))
-                calendar.write('{}{}{}\n'.format(year_number, month_number, day_number))
+                second = second.split('">')[1]
+                (day_name, day, month) = second.split(' ')
+                month = month_to_number(month)
+
+                calendar.write('{}{}\n'.format(
+                    collection_header.strip(), name))
+                date = datetime.strptime(
+                    '{}{}{}'.format(year, month, day), '%Y%m%d')
+                date += timedelta(days=1)
+                calendar.write('DTEND;VALUE=DATE:{}\n'.format(
+                    date.strftime('%Y%m%d')))
+                calendar.write(collection_footer)
+
+        calendar_footer = open('templates/calendar-footer.txt', 'r')
+        for line in calendar_footer.readlines():
+            calendar.write(line)

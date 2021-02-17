cab826b138bfa4067fc67076562f0f9004c5d75f
PanderMusubi <pander@users.sourceforge.net>
2021-02-18 00:06:27 +0100

    refactor and update

--- update.py
+++ update.py
@@ -1,101 +1,201 @@
 #!/usr/bin/env python3
+"""
+Generate calendars with garbage collection times in iCal format.
 
 # name: retrive.py
-# description: Generate calendars with garbage collection times in iCal format.
 # license: MIT
 
 # List all unique values of SUMMARY with following command
 # grep -rh SUMMARY ics|sort|uniq
+"""
 
-from datetime import datetime, timedelta
-from os import getpid, listdir, makedirs, path, rename, remove, sep
-#from os import chdir, symlink
+from datetime import datetime
+from os import getpid, listdir, makedirs, path, rename, sep
 from random import uniform, shuffle
 from socket import getfqdn
 from time import sleep, time
 from urllib import request
+import sys
 
 
 def month_to_number(month):
+    """
+    Blah blah.
+
+    Parameters
+    ----------
+    month : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    number : TYPE
+        DESCRIPTION.
+
+    """
+    number = ''
     if month.lower() == 'januari':
-        return '01'
-    elif month.lower() == 'februari':
-        return '02'
-    elif month.lower() == 'maart':
-        return '03'
-    elif month.lower() == 'april':
-        return '04'
-    elif month.lower() == 'mei':
-        return '05'
-    elif month.lower() == 'juni':
-        return '06'
-    elif month.lower() == 'juli':
-        return '07'
-    elif month.lower() == 'augustus':
-        return '08'
-    elif month.lower() == 'september':
-        return '09'
-    elif month.lower() == 'oktober':
-        return '10'
-    elif month.lower() == 'november':
-        return '11'
-    elif month.lower() == 'december':
-        return '12'
-    else:
-        return ''
+        number = '01'
+    if month.lower() == 'februari':
+        number = '02'
+    if month.lower() == 'maart':
+        number = '03'
+    if month.lower() == 'april':
+        number = '04'
+    if month.lower() == 'mei':
+        number = '05'
+    if month.lower() == 'juni':
+        number = '06'
+    if month.lower() == 'juli':
+        number = '07'
+    if month.lower() == 'augustus':
+        number = '08'
+    if month.lower() == 'september':
+        number = '09'
+    if month.lower() == 'oktober':
+        number = '10'
+    if month.lower() == 'november':
+        number = '11'
+    if month.lower() == 'december':
+        number = '12'
+    return number
+
 
 def address_to_path(address):
+    """
+    Blah blah.
+
+    Parameters
+    ----------
+    month : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    number : TYPE
+        DESCRIPTION.
+
+    """
     basename = address.replace('/', '-')
     decimals = basename[:4]
     letters = basename[4:6]
     number = basename[7:]
-    return 'ics{}{}{}{}{}{}.ics'.format(sep, decimals, sep, letters, sep, number)
+    return f'ics{sep}{decimals}{sep}{letters}{sep}{number}.ics'
 
-def address_to_path(address):
-    basename = address.replace('/', '-')
-    decimals = basename[:4]
-    letters = basename[4:6]
-    number = basename[7:]
-    return 'ics{}{}{}{}{}{}.ics'.format(sep, decimals, sep, letters, sep, number)
 
 def address_to_dir(address):
+    """
+    Blah blah.
+
+    Parameters
+    ----------
+    month : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    number : TYPE
+        DESCRIPTION.
+
+    """
     basename = address.replace('/', '-')
     decimals = basename[:4]
     letters = basename[4:6]
-    return 'ics{}{}{}{}'.format(sep, decimals, sep, letters)
+    return f'ics{sep}{decimals}{sep}{letters}'
+
 
 def address_to_file(address):
+    """
+    Blah blah.
+
+    Parameters
+    ----------
+    month : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    number : TYPE
+        DESCRIPTION.
+
+    """
     basename = address.replace('/', '-')
     number = basename[7:]
-    return '{}.ics'.format(number)
+    return f'{number}.ics'
+
 
 def reminder_to_alarm(reminder):
+    """
+    Blah blah.
+
+    Parameters
+    ----------
+    month : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    number : TYPE
+        DESCRIPTION.
+
+    """
+    alarm = ''  # no alarm
     if reminder == 'T10H30M':  # maximum, i.e. 21:30 previous day
-        return '_2130'
+        alarm = '_2130'
     elif reminder == 'T9H15M':  # nine hours and a quarter before
-        return '_2215'
+        alarm = '_2215'
     elif reminder == 'T1H':  # one hour before 08:00
-        return '_0700'
+        alarm = '_0700'
     elif reminder == 'T30M':  # half hour before 08:00
-        return '_0730'
-    return ''  # no alarm
+        alarm = '_0730'
+    return alarm
+
 
 def improve_name(name):
+    """
+    Blah blah.
+
+    Parameters
+    ----------
+    month : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    number : TYPE
+        DESCRIPTION.
+
+    """
     if name == 'Groente, fruit- en tuinafval':
         name = 'Groente, Fruit- en Tuinafval (GFT)'
     elif name == 'Papier en karton':
         name = 'Papier en Karton'
     elif name == 'PMD':
         name = 'Plastic, Metalen en Drankkartons (PMD)'
-    return name.replace(',', '\,').replace(' & ', ' en ')
+    return name.replace(',', '\\,').replace(' & ', ' en ')
+
 
 def write_mad(data, event_seq, names):
+    """
+    Blah blah.
+
+    Parameters
+    ----------
+    month : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    number : TYPE
+        DESCRIPTION.
+
+    """
     for reminder in reminders:
         alarm = reminder_to_alarm(reminder)
-        temp = 'ics{}{}{}{}{}{}{}.tmp.ics'.format(sep, decimals, sep, letters, sep, number, alarm)
+        temp = f'ics{sep}{decimals}{sep}{letters}{sep}{number}{alarm}.tmp.ics'
         calendar = open(temp, 'w', newline='\r\n')
 
-        calendar_header = open('templates{}calendar-header.txt'.format(sep))
+        calendar_header = open(f'templates{sep}calendar-header.txt')
         for line in calendar_header:
             calendar.write(line)
 
@@ -128,16 +228,16 @@ def write_mad(data, event_seq, names):
                 else:
                     (day_name, day, month) = datum
                 month = month_to_number(month)
-   
-                calendar.write('{}{}\n'.format(
-                    collection_header.strip(), name))
+
+                calendar.write(f'{collection_header.strip()}{name}\n')
 
                 # write UID and autoincrement
-                calendar.write(uid_format % (dict(list(uid_replace_values.items()) + list({ 'lang': 'en', 'seq': event_seq }.items()))))
+                calendar.write(uid_format % (dict(
+                    list(uid_replace_values.items()) +
+                    list({'lang': 'en', 'seq': event_seq}.items()))))
                 event_seq += 1
 
-                date = datetime.strptime(
-                    '{}{}{}'.format(year, month, day), '%Y%m%d')
+                date = datetime.strptime(f'{year}{month}{day}', '%Y%m%d')
                 calendar.write('DTSTART;VALUE=DATE-TIME:{}T080000\n'.format(
                     date.strftime('%Y%m%d')))
                 calendar.write('DTEND;VALUE=DATE-TIME:{}T080000\n'.format(
@@ -149,25 +249,41 @@ def write_mad(data, event_seq, names):
                 if reminder != '':
                     calendar.write('BEGIN:VALARM\n')
                     calendar.write('ACTION:DISPLAY\n')
-                    calendar.write('TRIGGER;VALUE=DURATION:-P{}\n'.format(reminder))
-                    calendar.write('DESCRIPTION:{} aan de straat zetten\n'.format(name))
+                    calendar.write(f'TRIGGER;VALUE=DURATION:-P{reminder}\n')
+                    calendar.write(f'DESCRIPTION:{name} aan de straat'
+                                   ' zetten\n')
                     calendar.write('END:VALARM\n')
 
                 calendar.write(collection_footer)
 
-        calendar_footer = open('templates{}calendar-footer.txt'.format(sep))
+        calendar_footer = open(f'templates{sep}calendar-footer.txt')
         for line in calendar_footer:
             calendar.write(line)
-        rename(temp, '{}'.format(temp.replace('.tmp.ics', '.ics')))
+        rename(temp, temp.replace('.tmp.ics', '.ics'))
     return event_seq
 
+
 def write_rmn(data, event_seq, names):
+    """
+    Blah blah.
+
+    Parameters
+    ----------
+    month : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    number : TYPE
+        DESCRIPTION.
+
+    """
     for reminder in reminders:
         alarm = reminder_to_alarm(reminder)
-        temp = 'ics{}{}{}{}{}{}{}.tmp.ics'.format(sep, decimals, sep, letters, sep, number, alarm)
+        temp = f'ics{sep}{decimals}{sep}{letters}{sep}{number}{alarm}.tmp.ics'
         calendar = open(temp, 'w', newline='\r\n')
 
-        calendar_header = open('templates{}calendar-header.txt'.format(sep))
+        calendar_header = open(f'templates{sep}calendar-header.txt')
         for line in calendar_header:
             calendar.write(line)
 
@@ -204,13 +320,12 @@ def write_rmn(data, event_seq, names):
                         index += 1
                     name = line.split('alt="')[1].split('"')[0]
                     name = improve_name(name)
-                    calendar.write('{}{}\n'.format(
-                        collection_header.strip(), name))
-     
+                    calendar.write(f'{collection_header.strip()}{name}\n')
+
                     # write UID and autoincrement
-                    calendar.write(uid_format % (dict(list(uid_replace_values.items()) + list({ 'lang': 'en', 'seq': event_seq }.items()))))
+                    calendar.write(uid_format % (dict(list(uid_replace_values.items()) + list({'lang': 'en', 'seq': event_seq}.items()))))
                     event_seq += 1
-     
+
                     date = datetime.strptime(
                         '{}{}{}'.format(year, month, day), '%Y%m%d')
                     calendar.write('DTSTART;VALUE=DATE-TIME:{}T080000\n'.format(
@@ -224,10 +339,12 @@ def write_rmn(data, event_seq, names):
                     if reminder != '':
                         calendar.write('BEGIN:VALARM\n')
                         calendar.write('ACTION:DISPLAY\n')
-                        calendar.write('TRIGGER;VALUE=DURATION:-P{}\n'.format(reminder))
-                        calendar.write('DESCRIPTION:{} aan de straat zetten\n'.format(name))
+                        calendar.write('TRIGGER;VALUE=DURATION:'
+                                       f'-P{reminder}\n')
+                        calendar.write('DESCRIPTION:'
+                                       f'{name} aan de straat zetten\n')
                         calendar.write('END:VALARM\n')
-     
+
                     calendar.write(collection_footer)
 
         calendar_footer = open('templates{}calendar-footer.txt'.format(sep))
@@ -236,7 +353,22 @@ def write_rmn(data, event_seq, names):
         rename(temp, '{}'.format(temp.replace('.tmp.ics', '.ics')))
     return event_seq
 
+
 def write_rova(data, event_seq, names):
+    """
+    Blah blah.
+
+    Parameters
+    ----------
+    month : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    number : TYPE
+        DESCRIPTION.
+
+    """
     # scraping like it's 1999
     index = 0
     dates = None
@@ -249,12 +381,12 @@ def write_rova(data, event_seq, names):
         if '"kalenderContainer"' in line:
             dates = line.split('afvalkalenderData:\'')[1].split('\'')[0]
             break
-    if dates == None:
-        return event_seq #FIXME login via post with postcode required, also do logout! also set 'do not remember'
-    
+    if dates is None:
+        return event_seq  #FIXME login via post with postcode required, also do logout! also set 'do not remember'
+
     for reminder in reminders:
         alarm = reminder_to_alarm(reminder)
-        temp = 'ics{}{}{}{}{}{}{}.tmp.ics'.format(sep, decimals, sep, letters, sep, number, alarm)
+        temp = f'ics{sep}{decimals}{sep}{letters}{sep}{number}{alarm}.tmp.ics'
         calendar = open(temp, 'w', newline='\r\n')
 
         calendar_header = open('templates{}calendar-header.txt'.format(sep))
@@ -264,14 +396,14 @@ def write_rova(data, event_seq, names):
         print('TODO')
         for d in dates.split(','):
             print(d)
-            exit(0)#TODO, see FIXME above
-                
+            sys.exit(0)  # TODO, see FIXME above
+
     return event_seq
 
 
 # reminders at, before and after
-#reminders = ('', 'T10H30M', 'T9H15M', 'T30M', 'T1H')
-reminders =  ('', 'T10H30M', 'T1H')
+# reminders = ('', 'T10H30M', 'T9H15M', 'T30M', 'T1H')
+reminders = ('', 'T10H30M', 'T1H')
 
 # date and time
 utcnow = datetime.utcnow()
@@ -280,23 +412,23 @@ dtstamp = utcnow.strftime('%Y%m%dT%H%M%SZ')
 now = time()
 
 # event UID
-uid_format='UID:%(date)s-%(pid)d-%(seq)04d-%(lang)s@%(domain)s\n'
+uid_format = 'UID:%(date)s-%(pid)d-%(seq)04d-%(lang)s@%(domain)s\n'
 uid_replace_values = {
     'date': dtstamp,
-    'pid':  getpid(),
+    'pid': getpid(),
     'domain': getfqdn()
 }
 event_seq = 1
 
 # create ICS header
 collection_header = ''
-event_header = open('templates{}event-header.txt'.format(sep))
+event_header = open(f'templates{sep}event-header.txt')
 for line in event_header:
     collection_header += line.replace('DTSTAMP:', 'DTSTAMP:{}'.format(dtstamp))
 
 # create ICS footer
 collection_footer = ''
-event_footer = open('templates{}event-footer.txt'.format(sep))
+event_footer = open(f'templates{sep}event-footer.txt')
 for line in event_footer:
     collection_footer += line
 
@@ -333,9 +465,9 @@ for address in addresses:
     decimals = basename[:4]
     letters = basename[4:6]
     number = basename[7:]
-    dir = address_to_dir(address)
-    if not path.exists(dir):
-        makedirs(dir, exist_ok=True)
+    directory = address_to_dir(address)
+    if not path.exists(directory):
+        makedirs(directory, exist_ok=True)
 
     postcode = decimals + letters
     huisnummer = number.split('-')[0]
@@ -344,27 +476,31 @@ for address in addresses:
         toevoeging = number.split('-')[1]
 
     source = None
-    url = 'http://www.mijnafvalwijzer.nl/nl/{}/'.format(address)
+    url = f'http://www.mijnafvalwijzer.nl/nl/{address}/'
     try:
         data = request.urlopen(url).read().decode('utf-8').split('\n')
         source = 'maw'
     except:
-        url = 'https://inzamelschema.rmn.nl/adres/{}/jaarkalender'.format(address.replace('/', ':'))
+        url = 'https://inzamelschema.rmn.nl/adres/{}/jaarkalender'.format(
+            address.replace('/', ':'))
         try:
             data = request.urlopen(url).read().decode('utf-8').split('\n')
             source = 'rmn'
         except Exception as e:
-            url = 'http://afvalkalender.rova.nl/rest/login?postcode={}&huisnummer={}&toevoeging={}'.format(postcode, huisnummer, toevoeging)
+            url = 'http://afvalkalender.rova.nl/rest/login?' \
+                f'postcode={postcode}&huisnummer={huisnummer}&' \
+                f'toevoeging={toevoeging}'
             try:
                 data = request.urlopen(url).read().decode('utf-8').split('\n')
                 source = 'rova'
             except Exception as e:
                 url = 'https://www.afvalstoffendienst.nl/login' #TODO
                 try:
-                    data = request.urlopen(url).read().decode('utf-8').split('\n')
+                    data = request.urlopen(url).read().decode('utf-8').split(
+                        '\n')
                     source = 'asd'
                 except Exception as e:
-                    print('WARNING: Could not retrieve url {} because {}'.format(url, e))
+                    print(f'WARNING: Could not retrieve url {url} because {e}')
                     continue
     if source == 'maw':
         event_seq = write_mad(data, event_seq, names)
@@ -373,24 +509,8 @@ for address in addresses:
     elif source == 'rova':
         event_seq = write_rova(data, event_seq, names)
     else:
-        print('ERROR: Unknown source {}, skipping'.format(source))
+        print(f'ERROR: Unknown source {source}, skipping')
         continue
-
-
-# doesn't work on GitHub, see https://stackoverflow.com/questions/954560/how-does-git-handle-symbolic-links
-# for main, links in sorted(addresses.items()):
-#     main_file = address_to_file(main)
-#     chdir(address_to_dir(main))
-#     for link in links:
-#         for reminder in reminders:
-#             link_file = address_to_file(link)
-#             alarm = reminder_to_alarm(reminder)
-#             src = link_file.replace('.ics', '{}.ics'.format(alarm))
-#             dst = main_file.replace('.ics', '{}.ics'.format(alarm))
-#             if path.exists(src):
-#                 remove(src)
-#             symlink(dst, src)
-#     chdir('..{}..{}..'.format(sep, sep))
 
 if names:
     print('\nFound following types:')
@@ -400,10 +520,10 @@ if names:
         names_used.write('{}\n'.format(name.replace('\\', '')))
 
 for decimals in sorted(listdir('ics')):
-    for letters in sorted(listdir('ics{}{}'.format(sep, decimals))):
-        readme = open('ics{}{}{}{}{}README.md'.format(sep, decimals, sep, letters, sep), 'w')
-        readme.write('# URL\'s postcode {} {}\n\n'.format(decimals, letters))
-        for number in sorted(listdir('ics{}{}{}{}'.format(sep, decimals, sep, letters))):
+    for letters in sorted(listdir(f'ics{sep}{decimals}')):
+        readme = open(f'ics{sep}{decimals}{sep}{letters}{sep}README.md', 'w')
+        readme.write(f'# URL\'s postcode {decimals} {letters}\n\n')
+        for number in sorted(listdir(f'ics{sep}{decimals}{sep}{letters}')):
             if number == 'README.md':
                 continue
             number = number.replace('.ics', '')
@@ -411,6 +531,7 @@ for decimals in sorted(listdir('ics')):
                 alarm = number.split('_')[1]
                 readme.write('## Huisnummer {} met alarm om {}.{} uur\n\n'.format(number.split('_')[0], alarm[:2], alarm[2:]))
             else:
-                readme.write('## Huisnummer {} zonder alarm\n\n'.format(number))
-            readme.write('https://raw.github.com/PanderMusubi/afvalophaaldata/master/ics/{}/{}/{}.ics\n\n'.format(decimals, letters, number))
+                readme.write(f'## Huisnummer {number} zonder alarm\n\n')
+            readme.write('https://raw.github.com/PanderMusubi/afvalophaaldata/'
+                         f'master/ics/{decimals}/{letters}/{number}.ics\n\n')
 #            readme.write('![QR-code https://raw.github.com/PanderMusubi/afvalophaaldata/master/ics/{}/{}/{}.ics](https://api.qrserver.com/v1/create-qr-code/?data=https%3A%2F%2Fraw.github.com%2FPanderMusubi%2Fafvalophaaldata%2Fmaster%2Fics%2F{}%2F{}%2F{}.ics)\n\n'.format(decimals, letters, number, decimals, letters, number))
